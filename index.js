dd773a0c61dbd557bb2c83614fb402be7437a841
Dalya <LocalAdmin@16DCompL069.campus.ncl.ac.uk>
2018-11-19 16:36:57 +0000

    added manual origin detection

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-require('dotenv').config();
-const http = require("http"),
-  server = http.createServer().listen(3000);
+require("dotenv").config();
+const http = require("http");
+const httpProxy = require("http-proxy");
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer().listen(3123);
 
+const baseUrl = "api.github.com";
 
-const baseUrl = "https://api.github.com";
+// const allowedOrigins = [ RegExp('^(www\.){0,}not-dalia.github.io/relevant-space/*'),  ];
+const allowedOrigins = [
+  RegExp("^http(?:s)?:\/\/not-dalia.github.io/*"),
+  RegExp("^http(?:s)?:\/\/localhost:4000/*")
+];
 
 server.on("request", (req, res) => {
-  var connector = http.request(
-    {
-      host: baseUrl,
-      path: req.url,
-      method: req.method,
-      headers: {...req.headers, Authorization: `token ${process.env.GH_PAT}`}
-    },
-    resp => {
-      resp.pipe(res);
-    }
-  );
-
-  req.pipe(connector);
+  let isOriginAllowed = checkRequestOrigin(req.headers.origin);
+  if (!isOriginAllowed) {
+    res.statusCode = 403;
+    res.end();
+    return;
+  }
+  req.headers.host = baseUrl;
+  req.headers.Authorization = `token ${process.env.GH_PAT}`;
+  proxy.web(req, res, { target: `https://${baseUrl}` });
 });
+
+proxy.on('proxyRes', function(proxyRes, req, res) {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+});
+
+function checkRequestOrigin(origin) {
+  let originAllowed = false;
+  if (!origin) return originAllowed;
+  for (let i in allowedOrigins) {
+    if (checkAllowedOrigins(origin, allowedOrigins[i])){
+      originAllowed = true;
+      break;
+    }
+  }
+  return originAllowed;
+}
+
+function checkAllowedOrigins(origin, allowedOrigin){
+  if (typeof allowedOrigin === 'string' || allowedOrigin instanceof String) {
+    return origin === allowedOrigin;
+  } else if (allowedOrigin instanceof RegExp) {
+    return allowedOrigin.test(origin);
+  } else {
+    return !!allowedOrigins;
+  }
+}
